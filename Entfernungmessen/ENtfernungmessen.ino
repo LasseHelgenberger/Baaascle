9e4ca2e9abf70df738fc9c4f4d3b461a5a466da9
Pascal Henningsen <henningsen.pascal@gmail.com>
2019-01-19 13:02:29 +0100

    Entfernungmessen hinzugefügt

--- Entfernungmessen/ENtfernungmessen.ino
+++ Entfernungmessen/ENtfernungmessen.ino
@@ -3,6 +3,7 @@ void setup() {
   // put your setup code here, to run once:
   //pin 5 tricker schall losschicken
   //pin 4 empfängt schall
+  Serial.begin(9600);
   pinMode(5,OUTPUT);
   pinMode(4,INPUT);
 }
@@ -10,9 +11,13 @@ void setup() {
 void loop() {
   // put your main code here, to run repeatedly:
   digitalWrite(5,HIGH);
+  delay(5);
+  digitalWrite(5,LOW);
   while(digitalRead(4)==LOW){
       zeit=zeit+1;
     }
   Serial.print(zeit);
   digitalWrite(5,LOW);
+  delay(5000);
+  zeit=0;
 }
